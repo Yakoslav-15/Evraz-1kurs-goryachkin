6130d15aa45ef36563e6c351e2f1a7e56304258d
yaki <Yarjslavgoryachkin3000@gmailcom>
2024-11-06 17:59:47 +0300

    7 занятие

--- 6 занятие.js
+++ 6 занятие.js
@@ -1,4 +1,44 @@
 let nameDOM = document.getElementById('name');
-let name = '';
-
+let name = prompt('Укажите ваше имя');
+console.log(name)
 nameDOM.innerText = name;
+
+let lastnameDom = document.getElementById('lastname')
+let lastname = prompt('Укажите вашу фамилию')
+console.log(lastname)
+lastnameDom.innerText = lastname
+
+let middlenameDom = document.getElementById('middlename')
+let middlename = prompt('Укажите ваше отчество')
+console.log(middlename)
+middlenameDom.innerText = middlename
+
+let ageDom = document.getElementById('age')
+let age = prompt('Укажите сколько вам лет')
+console.log(age)
+ageDom.innerText = age
+
+let schoolDom = document.getElementById('school')
+let school = prompt('Укажите вашу школу')
+console.log(school)
+schoolDom.innerText = school
+
+let classDom = document.getElementById('class')
+let classs = prompt('Укажите ваш класс')
+console.log(classs)
+classDom.innerText = classs
+
+let sportDom = document.getElementById('sport')
+let sport = prompt('Укажите сколько ваш любимый вид спорта')
+console.log(sport)
+sportDom.innerText = sport
+
+let subjectDom = document.getElementById('subject')
+let subject = prompt('Укажите ваш любимый предмет в школе')
+console.log(subject)
+subjectDom.innerText = subject
+
+let colorDom = document.getElementById('color')
+let color = prompt('Укажите ваш любимый цвет')
+console.log(color)
+colorDom.innerText = color
---
+++ 7 занятие.js
@@ -0,0 +1,30 @@
+function clearDiv() {
+    let div = document.getElementById('clear')
+    div.innerText = '';
+}
+function make() {
+    let body = document.getElementById('sdf')
+    body.style.background ='red'
+}
+
+
+function make() {
+    let body = document.getElementById('sdf')
+    body.style.background ='green'
+}
+
+function make() {
+    let body = document.getElementById('sdf')
+    body.style.background ='blue'
+}
+
+
+
+
+
+
+
+
+
+
+
