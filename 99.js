2a5c2f325f1d0d68fb5631626b96cec92464dd28
yaki <Yarjslavgoryachkin3000@gmailcom>
2024-12-04 17:33:16 +0300

    55

--- 99.js
+++ 99.js
@@ -1,3 +1,50 @@
-function teils() {
+let a = 0
+let total= document.getElementById('total')
+function make() {
+    let total= document.getElementById('total')
+    total.style =''
+}
+let a = 1
+function make2() {
+let total = document.getElementById('total')
+total.style = ''
+    a = 1
+}
+
+let a = 2
+function make3() {
+    let total = document.getElementById('total')
+d = 2
+}
+let a = 5
+function make4() {
+    let total = document.getElementById('total')
+total.style = ''
 
 }
+let a
+function make5() {
+    let total = document.getElementById('total')
+    total.style = ''
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
