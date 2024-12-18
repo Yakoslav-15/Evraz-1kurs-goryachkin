754f7f567988d76ccb9c902cb588c41e99eb0385
yaki <Yarjslavgoryachkin3000@gmailcom>
2024-12-18 17:53:39 +0300

    5565y757

--- 100.js
+++ 100.js
@@ -1,12 +1,22 @@
 
-        function check() {
-            a = document.getElementById('g1').value;
+        function checkEvenness() {
+        const number = document.getElementById('numberInput').value;
+        const resultText = document.getElementById('result');
+        if (number.trim() === "") {
+        resultText.textContent = "Пожалуйста, введите число!";
+        return;
+    }
 
-            if () {
-                (a % 2 === 0)
-                console.log('число четное')
-            } else if () {
-                (a % 2 === 1)
-                console.log('число нечетное')
-            }
-        }
+        const num = parseInt(number, 10);
+        if (isNaN(num)) {
+        resultText.textContent = "Введено не число. Попробуйте снова.";
+    } else {
+        if (num % 2 === 0) {
+        resultText.textContent = `Число чётное.`;
+    } else {
+        resultText.textContent = `Число нечётное.`;
+    }
+    }
+    }
+
+
---
+++ 101.js
@@ -0,0 +1,44 @@
+
+let input1 = document.getElementById('inp1');
+let input2 = document.getElementById('inp2');
+
+let divinput1 = document.getElementById('divinp1');
+let divinput2 = document.getElementById('divinp2');
+
+let result = document.getElementById('res');
+
+function checkNumber() {
+
+    divinput1.classList.remove('higher');
+    divinput2.classList.remove('higher');
+
+
+    let a = +input1.value;
+    let b = +input2.value;
+
+    let answer = ''
+if (a > b) {
+    console.log('первое число больше, чем второе')
+}
+else if (a < b) {
+    console.log('второе число больше, чем первое')
+}
+else if(a == b) {
+    console.log('числа равные друг другу')
+}
+
+
+    /**
+     * Это еще один вид комментариев, позволяющий делать многострочные комментарии.
+     * Как указанно в комментарии ниже, вам ниже необходимо написать свой код, своё условие для реализации данной задачи.
+     *
+     * Чтобы сделать как на гифке выделение зеленым цветом инпута, где указано большее число, необходимо воспользоваться командой:
+     * divinput1.classList.add('higher'); // это для первого блока
+     * divinput2.classList.add('higher'); // это для второго блока
+     *
+     * Обратите внимание, отличается только переменная, к которой мы обращаемся (divinput1 или dininput2), которые были созданы в начале файла
+     */
+
+    // ЗДЕСЬ НЕОБХОДИМО НАПИСАТЬ УСЛОВИЯ (для вас уже готовы переменные a и b, не забудьте)
+
+    result.innerText = answer;
