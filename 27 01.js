c802fb75018eef3170c63e77d47abebce7b041bc
yaki <Yarjslavgoryachkin3000@gmailcom>
2025-01-29 17:48:01 +0300

    22\05\01

--- 27 01.js
+++ 27 01.js
@@ -13,6 +13,31 @@ function addLi() {
 
         addElement.append(liElement);
 
+
         input.value = '';
     }
 }
+
+function addLi2() {
+    let addElement = document.getElementById('add2');
+    let input = document.getElementById('name2')
+    let liElement = document.createElement('li');
+
+    if (input.value === '') {
+        alert('Добавь значение')
+    }
+    else {
+        liElement.innerText = input.value;
+
+
+
+        addElement.append(liElement);
+
+        input.value = '';
+    }
+
+}
+
+
+
+
