2e222c7c8d2517dc6f8900134179f6a77e4b78ad
yaki <Yarjslavgoryachkin3000@gmailcom>
2025-02-12 17:59:20 +0300

    2025_02_12 устное собеседование

--- 27 01.js
+++ 27 01.js
@@ -1,47 +1,45 @@
 function addLi() {
     let addElement = document.getElementById('add');
     let input = document.getElementById('name')
-    let liElement = document.createElement('li');
+    let input2 = document.getElementById('name2')
     if (input.value === '') {
         alert('Добавь значение')
     }
     else {
-        liElement.innerText = input.value;
-
-
-
-        addElement.append(liElement);
-
-
-        input.value = '';
-
+        if (input2.value === '') {
+            alert('Добавь количество повторов')
+        }
+        else {
+            for (let i = 0; i < +input2.value; i++) {
+                let liElement = document.createElement('li');
+                liElement.innerText = input.value;
+                addElement.append(liElement);
+            }
+            input.value = '';
+        }
     }
 }
 
 function addLi2() {
-    let addElement = document.getElementById('add2');
-    let input = document.getElementById('name2')
-    let liElement = document.createElement('li');
-
-    if (input.value === '') {
+    let add2Element = document.getElementById('add2');
+    let input3 = document.getElementById('name3')
+    let input4 = document.getElementById('name4')
+    if (input3.value === '') {
         alert('Добавь значение')
     }
     else {
-        liElement.innerText = input.value;
-
-
-
-        addElement.append(liElement);
-
-        input.value = '';
+        if (input4.value === '') {
+            alert('Добавь количество повторов')
+        } else {
+            for (let i = 0; i < +input4.value; i++) {
+                let liElement = document.createElement('li');
+                liElement.innerText = input3.value;
+                add2Element.append(liElement);
+            }
+            input3.value = '';
+        }
     }
-
 }
-
-
-
-
-
 
 
 
