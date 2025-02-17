d59d5f4560bde59be705215c15a84912a968c6c2
yaki <Yarjslavgoryachkin3000@gmailcom>
2025-02-17 15:58:53 +0300

    17/02/25

--- 10.02.js
+++ 10.02.js
@@ -1,3 +1,30 @@
-function addLi1 {
-
+let list = []
+function addLi1() {
+    let span1 = document.getElementById('span1');
+    for (let i = 0; i < 5; i++) {
+        span1.innerText = span1.innerText + i;
+    }
 }
+
+
+function addLi2() {
+    let span1 = document.getElementById('span1');
+    for (let i = 1; i < 6; i++) {
+        span1.innerText = span1.innerText + i;
+    }
+}
+
+function addLi3() {
+    let span1 = document.getElementById('span1');
+    for (let i = 0; i < 10; i++) {
+        span1.innerText = span1.innerText + i;
+    }
+}
+
+function addLi4() {
+    let span1 = document.getElementById('span1');
+    for (let i = 1; i < 11; i++) {
+        span1.innerText = span1.innerText + i;
+    }
+}
+
