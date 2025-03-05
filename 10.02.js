4fb0e6d87aad5bea9218cf2e084d72f7adf878fa
yaki <Yarjslavgoryachkin3000@gmailcom>
2025-03-05 18:00:22 +0300

    2025

--- 10.02.js
+++ 10.02.js
@@ -2,29 +2,53 @@ let list = []
 function addLi1() {
     let span1 = document.getElementById('span1');
     for (let i = 0; i < 5; i++) {
-        span1.innerText = span1.innerText + i;
+        span1.innerText = span1.innerText + " " + i;
     }
 }
 
 
 function addLi2() {
-    let span1 = document.getElementById('span1');
+    let span2 = document.getElementById('span2');
     for (let i = 1; i < 6; i++) {
-        span1.innerText = span1.innerText + i;
+        span2.innerText = span2.innerText + " " + i;
     }
 }
 
 function addLi3() {
-    let span1 = document.getElementById('span1');
+    let span3 = document.getElementById('span3');
     for (let i = 0; i < 10; i++) {
-        span1.innerText = span1.innerText + i;
+        span3.innerText = span3.innerText + " " + i;
     }
 }
 
 function addLi4() {
-    let span1 = document.getElementById('span1');
+    let span4 = document.getElementById('span4');
     for (let i = 1; i < 11; i++) {
-        span1.innerText = span1.innerText + i;
+        span4.innerText = span4.innerText + " " + i;
     }
 }
+
+
+function addLi5() {
+    let span5 = document.getElementById('span5');
+    for (let i = 0; i < 20; i++) {
+        if 
+        span5.innerText = span5.innerText + " " + i;
+    }
+}
+
+function addLi6() {
+    let span6 = document.getElementById('span6');
+    for (let i = 1; i < 11; i++) {
+        span6.innerText = span6.innerText + " " + i;
+    }
+}
+
+function addLi7() {
+    let span7 = document.getElementById('span7');
+    for (let i = 1; i < 11; i++) {
+        span7.innerText = span7.innerText + " " + i;
+    }
+}
+
 
